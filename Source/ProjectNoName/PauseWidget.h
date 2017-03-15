8af0537e96a2f0528fb1bf10e41aff8ceb3058e7
Antoine Cormery <antoine@cormery.eu>
2017-03-15 00:34:25 -0400

    integrate pauseHUD, but need to fix crash

---
+++ Source/ProjectNoName/PauseWidget.h
@@ -0,0 +1,24 @@
+
+
+#pragma once
+#include "Blueprint/UserWidget.h"
+#include "Runtime/UMG/Public/Components/ContentWidget.h"
+#include "Runtime/UMG/Public/Components/Button.h"
+#include "PauseWidget.generated.h"
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class PROJECTNONAME_API UPauseWidget: public UUserWidget
+{
+    GENERATED_BODY()
+    
+private:
+    UButton* _playButton;
+    UButton* _returnMainMenuButton;
+    
+public:
+    void Init();
+    void PlayButtonClicked();
+    void ReturnMainMenuButtonCliked();
+};
